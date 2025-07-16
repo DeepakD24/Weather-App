dab6c59e252ec3c61dcb333202c54c54138222f3
DeepakD24 <deepakkalaivanidk@gmail.com>
2025-07-16 10:29:27 +0530

    Setup Django

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -13,9 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xt+-vp*yr2#-asz@y)j%omh-b!-0+lx&nx#_^=y&1^o1($aw(c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['https://weather-app-hme4.onrender.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['yourdomain.com','your-render-url.onrender.com','127.0.0.1' ,'localhost']
 
 
 # Application definition
@@ -109,6 +110,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
