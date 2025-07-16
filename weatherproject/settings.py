0c38791892aa7b3aba9498e37dc8887e0abea69d
DeepakD24 <deepakkalaivanidk@gmail.com>
2025-07-16 10:34:18 +0530

    Update Settings.py

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -10,10 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xt+-vp*yr2#-asz@y)j%omh-b!-0+lx&nx#_^=y&1^o1($aw(c'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')=='True'
 
 #ALLOWED_HOSTS = ['https://weather-app-hme4.onrender.com','127.0.0.1','localhost']
 ALLOWED_HOSTS = ['yourdomain.com','your-render-url.onrender.com','127.0.0.1' ,'localhost']
